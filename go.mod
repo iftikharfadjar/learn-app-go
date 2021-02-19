7e1f0811f1f42062f6e3ef42931385f3d61b5ac7
fadjar iftikhar <fadjar.iftikhar@gmail.com>
2021-02-19 14:32:58 +0700

    learning how to call mod

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/iftikharfadjar/learn-app-go
+
+go 1.15
+
+require github.com/iftikharfadjar/learngo v1.0.2
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	say_hello "github.com/iftikharfadjar/learngo"
+)
+
+func main(){
+	fmt.Println(say_hello.SayHello())
+}
